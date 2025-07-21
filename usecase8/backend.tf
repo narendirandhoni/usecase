9a028cd84e86d8276e887a61f55439367878cba7
narendiran <87845769+narendirandhoni@users.noreply.github.com>
2025-07-21 07:20:00 +0530

    Update backend.tf

--- usecase8/backend.tf
+++ usecase8/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "hcl-backend-mumbai "
+    bucket         = "hcl-backend-mumbai"
     key            = "usecase-8/terraform.tfstate"
     region         = "ap-south-1"                
     encrypt        = true 
