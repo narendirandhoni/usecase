d5e6dd17681147cbfa8a2b6c26945fa002b93de6
narendiran <87845769+narendirandhoni@users.noreply.github.com>
2025-07-21 06:36:51 +0530

    Create backend.tf

---
+++ usecase8/modules/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "hcl-backend-mumbai "
+    key            = "usecase-8/terraform.tfstate"
+    region         = "ap-south-1"                
+    encrypt        = true 
+    use_lockfile = true
+
+  }
+}
